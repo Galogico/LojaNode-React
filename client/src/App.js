1463a959cc07ed1a99e234a1a05f9ce3bfcb2757
galogico <galogico@gmail.com>
2023-04-18 16:22:54 -0300

    cadastro funcionando

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,28 @@
 import logo from './logo.svg';
 import Titulo from './components/Titulo';
+import { useState } from 'react';
 
-function App() {
+function App() {  
+  const [tarefas,setTarefas] = useState([]);
+  const [newTask,setNewTask] = useState('');
+  const addTarefa=()=>{
+    let newTarefa = newTask;
+    setTarefas([...tarefas, newTarefa])
+    setNewTask('')
+  }
   return (
     <div className="App">
         <Titulo/>
+        <input
+          value={newTask}
+          onChange={(e)=>setNewTask(e.target.value)}
+        />
+        <button onClick={addTarefa}>Cadastrar</button>
+        <div>
+          {
+            tarefas.map(t=><p key={t}>{t}</p>)
+          }
+        </div>
     </div>
   );
 }
