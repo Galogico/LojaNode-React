9c307eb8a09695ef896484182465cdec06432f95
galogico <galogico@gmail.com>
2023-04-25 16:17:57 -0300

    connect ok

--- server/database/connection.js
+++ server/database/connection.js
@@ -2,3 +2,16 @@ const mongoose = require("mongoose")
 
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASS;
+
+const connect =()=>{
+    mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@banco-aula.gxuhkm1.mongodb.net/test?retryWrites=true&w=majority`)
+    const connection = mongoose.connection;
+    connection.on("error",()=>{
+        console.log("erro ao conectar com o mongodb")
+    })
+
+    connection.on("open",()=>{
+        console.log("conectado com o mongodb")
+    })
+}
+connect()
