3cdb0e2fc804ed07e7ed86137845d0b6e8ee0cfd
RodrigoBCGomes <rodrigosgtau@gmail.com>
2023-05-09 16:02:45 -0300

    validacoes da requisicao

--- server/controllers/AuthRegisterUserController.js
+++ server/controllers/AuthRegisterUserController.js
@@ -5,6 +5,32 @@ module.exports = class AuthRegisterUserController{
         res.send({message:"Bem vindo à nossa API!"})
     }
     static async registerUser(req,res){
-        
-    }
+        const {
+            name,
+            email,
+            age,
+            password,
+            confirmPassword
+        } = req.body
+
+        let image = ""
+        if(req.file){
+            image = req.file.filename
+        }
+        if(!name){
+            return res.status(422).json({message: "o nome é obrigatório"})
+        }
+        if(!email){
+            return res.status(422).json({message: "o email é obrigatório"})
+        }
+        if(!age){
+            return res.status(422).json({message: "a idade é obrigatória"})
+        }
+        if(!password){
+            return res.status(422).json({message: "a senha é obrigatória"})
+        }
+        if(confirmPassword != password){
+            return res.status(422).json({message: "a senha está errado"})
+        }
+    }  
 }
