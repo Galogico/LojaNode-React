b64ec7e58d528d8f60cd61aaf26bb136af46c55a
galogico <galogico@gmail.com>
2023-05-16 16:05:39 -0300

    adicionando o botao de cadastrar

--- client/src/pages/Registrar.jsx
+++ client/src/pages/Registrar.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
-
+import { useState } from 'react'
+import axios from 'axios'
 function Registrar() {
+  const [user,setUser] = useState({
+                                    name: 'testando2',
+                                    email:'testando2@gmail.com',
+                                    age: 12,
+                                    password: 'super123',
+                                    confirmPassword:'super123'
+                                  })
+  function cadastrarUsuario(user){
+    axios.post('http://localhost:8081/auth/register/user',user)
+      .then(res=>console.log(res.data))
+      .catch(erro=> console.log(erro))
+  }
+
   return (
-    <div>saas</div>
+    <div>
+      <button onClick={()=>cadastrarUsuario(user)}>Cadastrar</button>
+    </div>
   )
 }
 
--- server/controllers/AuthRegisterUserController.js
+++ server/controllers/AuthRegisterUserController.js
@@ -1,58 +1,61 @@
 const User = require('../models/User')
+
 const bcrypt = require('bcrypt');
 
 module.exports = class AuthRegisterUserController{
-    static async init(req,res){
-        res.send({message:"Bem vindo à nossa API!"})
+  static async init(req,res){
+    res.send({message: "Bem vindo a nossa api!"})
+  }
+  static async registerUser(req,res){
+    const {
+      name,
+      email,
+      age,
+      password,
+      confirmPassword
+    }= req.body
+
+    let image = ""
+    if(req.file){
+      image = req.file.filename
     }
-    static async registerUser(req,res){
-        const {
-            name,
-            email,
-            age,
-            password,
-            confirmPassword
-        } = req.body
+    if(!name){
+      return res.status(422).json({message: "o nome é obrigatório"})
+    }
+    if(!email){
+      return res.status(422).json({message: "o email é obrigatório"})
+    }
+    if(!age){
+      return res.status(422).json({message: "a idade é obrigatória"})
+    }
+    if(!password){
+      return res.status(422).json({message: "a senha é obrigatória"})
+    }
+    if(confirmPassword != password){
+      return res.status(422).json({message: "As senhas não são iguais"})
+    }
 
-        let image = ""
-        if(req.file){
-            image = req.file.filename
-        }
-        if(!name){
-            return res.status(422).json({message: "o nome é obrigatório"})
-        }
-        if(!email){
-            return res.status(422).json({message: "o email é obrigatório"})
-        }
-        if(!age){
-            return res.status(422).json({message: "a idade é obrigatória"})
-        }
-        if(!password){
-            return res.status(422).json({message: "a senha é obrigatória"})
-        }
-        if(confirmPassword != password){
-            return res.status(422).json({message: "a senha está errada"})
-        }
+    const userExist = await User.findOne({email: email})
 
-        const userExist = await User.findOne({email: email})
-        if(userExist){
-            return res.status(422).json({message: "Ja tem usuario com seu email"})
-        }
-        const hash = await bcrypt(12)
+    if(userExist){
+      return res.status(422).json({message: "Já tem um usuário com teu email"})
+    }
+    const hash = await bcrypt.genSalt(12)
 
-        const passwordHash = await bcrypt.hash(password,hash)
+    const passwordHash = await bcrypt.hash(password, hash)
 
-        const user = new User({
-            name,
-            email,
-            age,
-            image,
-            password: passwordHash
-        })
-        try{
-            res.status(201).json({message: "usuário cadastrado com sucesso", user})
-        } catch (error) {
-            res.status(500).json({message: "Ocorreu um erro ao cadastrar um usuário, tente novamente mais tarde"})
-        }
-    }  
+    const user = new User({
+      name,
+      email,
+      age,
+      image,
+      password: passwordHash
+    })
+    try {
+      await user.save()
+      res.status(201).json({message: "Usuário cadastrado com sucesso",user})
+    } catch (error) {
+      res.status(500).json({message: "Ocorreu um erro ao cadastrar o usuário, tente novamente mais tarde"})
+    }
+  }
 }
