b68771ccf50b5068ff6964d74a1bbf2b4a428bf6
galogico <galogico@gmail.com>
2023-05-23 16:28:43 -0300

    finalizando card

--- client/src/pages/Registrar.jsx
+++ client/src/pages/Registrar.jsx
@@ -29,63 +29,62 @@ function Registrar() {
   }
   return (
     <div>
-
-    <form className='form' onSubmit={envioFormulario}>
-    <div>
-      <label className='label' htmlFor="nome">Nome:</label>
-      <input
-        className='input' 
-        type="text" 
-        id="nome" 
-        value={user.name}
-        onChange={(e)=>setUser({...user,name: e.target.value})}
-      />
-    </div>
-    <div>
-      <label className='label' htmlFor="email">Email:</label>
-      <input
-        className='input' 
-        type="text" 
-        id="email" 
-        value={user.email}
-        onChange={(e)=>setUser({...user,email: e.target.value})}
+      <form className='form' onSubmit={envioFormulario}>
+      <div>
+        <label className='label' htmlFor="nome">Nome:</label>
+        <input
+          className='input' 
+          type="text" 
+          id="nome" 
+          value={user.name}
+          onChange={(e)=>setUser({...user,name: e.target.value})}
         />
-    </div>
-    <div>
-      <label className='label' htmlFor="idade">Idade:</label>
-      <input
-        className='input' 
-        type="number" 
-        id="idade" 
-        value={user.age}
-        onChange={(e)=>setUser({...user,age: e.target.value})}
-        />
-    </div>
-    <div>
-      <label className='label' htmlFor="senha">Senha:</label>
-      <input
-        className='input' 
-        type="password" 
-        id="senha" 
-        value={user.password}
-        onChange={(e)=>setUser({...user,password: e.target.value})}
-        />
-    </div>
-      <label className='label' htmlFor="confirmPassword">Confirme Sua Senha:</label>
-      <input
-        className='input' 
-        type="password" 
-        id="confirmPassword" 
-        value={user.confirmPassword}
-        onChange={(e)=>setUser({...user,confirmPassword: e.target.value})}
-        />
+      </div>
+      <div>
+        <label className='label' htmlFor="email">Email:</label>
+        <input
+          className='input' 
+          type="text" 
+          id="email" 
+          value={user.email}
+          onChange={(e)=>setUser({...user,email: e.target.value})}
+          />
+      </div>
+      <div>
+        <label className='label' htmlFor="idade">Idade:</label>
+        <input
+          className='input' 
+          type="number" 
+          id="idade" 
+          value={user.age}
+          onChange={(e)=>setUser({...user,age: e.target.value})}
+          />
+      </div>
+      <div>
+        <label className='label' htmlFor="senha">Senha:</label>
+        <input
+          className='input' 
+          type="password" 
+          id="senha" 
+          value={user.password}
+          onChange={(e)=>setUser({...user,password: e.target.value})}
+          />
+      </div>
+        <label className='label' htmlFor="confirmPassword">Confirme Sua Senha:</label>
+        <input
+          className='input' 
+          type="password" 
+          id="confirmPassword" 
+          value={user.confirmPassword}
+          onChange={(e)=>setUser({...user,confirmPassword: e.target.value})}
+          />
+      
+      <button type='submit'>Cadastrar</button>
+    </form>
+    {
+      showCard ?<Popup/> : null
+    }
     
-    <button type='submit'>Cadastrar</button>
-  </form>
-  {
-    showCard ?<Popup/> : null
-  }
-  
   </div>
   )
 }
