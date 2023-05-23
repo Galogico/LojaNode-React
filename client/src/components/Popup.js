63f3d165b4d8c77f86d54fd2ad513c409a8a8893
galogico <galogico@gmail.com>
2023-05-23 16:24:18 -0300

    timer para o card sumir

---
+++ client/src/components/Popup.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './popup.css'
+
+function Popup() {
+  return (
+    <div className='popup'>
+
+
+    </div>
+  )
+}
+
+export default Popup
--- client/src/pages/Registrar.jsx
+++ client/src/pages/Registrar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState } from 'react'
 import axios from 'axios'
 import { ButtonFormCadastro, DivFormCadastro, InputFormCadastro, TextFormCadastro, ModeloTitulo } from '../styles/styles'
+import Popup from '../components/Popup'
 
 function Registrar() {
   const [user,setUser] = useState({
@@ -16,12 +17,19 @@ function Registrar() {
       .then(res=>console.log(res.data))
       .catch(erro=> console.log(erro))
   }
+  const [showCard,setShowCard]=useState(false);
 
   function envioFormulario(event){
     event.preventDefault();
     cadastrarUsuario(user);
+    setShowCard(true);
+    setTimeout(()=>{
+      setShowCard(false);
+    },3500)
   }
   return (
+    <div>
+
     <form className='form' onSubmit={envioFormulario}>
     <div>
       <label className='label' htmlFor="nome">Nome:</label>
@@ -41,7 +49,7 @@ function Registrar() {
         id="email" 
         value={user.email}
         onChange={(e)=>setUser({...user,email: e.target.value})}
-      />
+        />
     </div>
     <div>
       <label className='label' htmlFor="idade">Idade:</label>
@@ -51,7 +59,7 @@ function Registrar() {
         id="idade" 
         value={user.age}
         onChange={(e)=>setUser({...user,age: e.target.value})}
-      />
+        />
     </div>
     <div>
       <label className='label' htmlFor="senha">Senha:</label>
@@ -61,7 +69,7 @@ function Registrar() {
         id="senha" 
         value={user.password}
         onChange={(e)=>setUser({...user,password: e.target.value})}
-      />
+        />
     </div>
       <label className='label' htmlFor="confirmPassword">Confirme Sua Senha:</label>
       <input
@@ -70,10 +78,15 @@ function Registrar() {
         id="confirmPassword" 
         value={user.confirmPassword}
         onChange={(e)=>setUser({...user,confirmPassword: e.target.value})}
-      />
+        />
     
     <button type='submit'>Cadastrar</button>
   </form>
+  {
+    showCard ?<Popup/> : null
+  }
+  
+  </div>
   )
 }
 
