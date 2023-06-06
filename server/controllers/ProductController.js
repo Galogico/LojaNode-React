86bb9e8e2e5b02daa5784af87ea6a09233e48acc
galogico <galogico@gmail.com>
2023-06-06 16:28:55 -0300

    prevendo erros de tipagem

--- server/controllers/ProductController.js
+++ server/controllers/ProductController.js
@@ -14,6 +14,34 @@ module.exports= class ProductController{
     static async createProduct(req,res){
         try{
             const { nome,price,desc,disponivel } = req.body;
+            if(!nome){
+                return res.status(406).json({message: "o nome é obrigatório"})
+                
+            }
+            if(!price){
+                return res.status(406).json({message: "o preco é obrigatório"})
+            }
+            if(!desc){
+                return res.status(406).json({message: "A descricao é obrigatório"})
+        
+            }
+            if(!disponivel){
+                return res.status(406).json({message: "E necessario inoformar se o produto esta disponivel"})
+            }
+
+            if(disponivel !== Boolean){
+                return res.status(406).json({message: "o campo disponivel tem que ser booleano"})
+            }
+            if(price !== Number){
+                return res.status(406).json({message: "o campo disponivel tem que ser booleano"})
+            }
+            if(desc !== String){
+                return res.status(406).json({message: "o campo disponivel tem que ser booleano"})
+            }
+            if(nome !== String){
+                return res.status(406).json({message: "o campo disponivel tem que ser booleano"})
+            }
+
             const produto = new Product({nome,price,desc,disponivel})
             await produto.save()
             res.status(201).json(produto)
